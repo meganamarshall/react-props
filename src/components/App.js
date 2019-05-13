04a878deba9b3dfaf4d303ae932443272b034d9d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-13 16:20:40 -0700

    Added header, test passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Dog from './Dog';
+import Header from './Header';
 
 export default function App() {
   return (
     <>
+    <Header />
     <Dog name="Bear" age={8} weight="40 lbs" />
     </>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <img src="https://pbs.twimg.com/profile_images/378800000456437229/9f484daeea4fa1c86be80662e6a734e3_400x400.jpeg" />
+      <h1>my dog</h1>
+    </header>
+  );
+}
