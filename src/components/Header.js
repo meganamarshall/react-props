cea403b5b7178f2bab721285d994d013f87d0fe7
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-14 10:21:49 -0700

    added app test & header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <header>
       <h1>my dog</h1>
+      <img src="https://assets.caboosecms.com/media/236656_002-beagle_thumb.png?1512229005"></img>
     </header>
   );
 }
